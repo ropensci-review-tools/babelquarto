b79591b56b46d49bb96bf641bf469a52cbf6f169
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-05-25 15:37:21 +0200

    tweak

--- tests/testthat/test-register.R
+++ tests/testthat/test-register.R
@@ -13,7 +13,7 @@ test_that("register_main_language() works", {
   )
   book_path <- file.path(parent_dir, "blop")
   register_main_language("en", book_path)
-  expect_snapshot_file(file.path(book_path, "_quarto.yml"))
+  expect_snapshot_file(file.path(book_path, "_quarto.yml"), compare = testthat::compare_file_text)
 
   expect_snapshot(register_main_language("fr", book_path), error = TRUE)
 
@@ -43,7 +43,7 @@ test_that("register_further_languages() works", {
     file.path(book_path, "_quarto.yml"),
     file.path(book_path, "_quarto2.yml")
   )
-  expect_snapshot_file(file.path(book_path, "_quarto2.yml"))
+  expect_snapshot_file(file.path(book_path, "_quarto2.yml"), compare = testthat::compare_file_text)
 
   expect_snapshot(register_further_languages(c("es", "fr"), book_path))
 })
