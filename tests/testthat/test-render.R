826931a236725d6688a8214e4adba0e5d7a97693
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-04-12 12:39:02 +0200

    test: add tests for books with chapters inside a folder

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -39,6 +39,39 @@ test_that("render_book() works - change link", {
   expect_equal(xml2::xml_attr(spanish_link, "href"), "https://ropensci.org/es/index.es.html")
 })
 
+test_that("render_book() works -- chapters in folders", {
+  withr::local_envvar("BABELQUARTO_TESTS_URL" = "true")
+
+  parent_dir <- withr::local_tempdir()
+  project_dir <- "blop"
+  quarto_multilingual_book(
+    parent_dir = parent_dir,
+    project_dir = project_dir,
+    further_languages = c("es", "fr"),
+    main_language = "en"
+  )
+  qmds <- fs::dir_ls(file.path(parent_dir, project_dir), glob = "*.qmd")
+  qmds <- qmds[!grepl("index", fs::path_file(qmds))]
+  fs::dir_create(file.path(parent_dir, project_dir, "chapters"))
+  purrr::walk(
+    qmds, fs::file_move,
+    new_path = file.path(parent_dir, project_dir, "chapters")
+  )
+
+  config_lines <- brio::read_lines(file.path(parent_dir, project_dir, "_quarto.yml"))
+  config_lines <- gsub("- (.*)\\.qmd", "- chapters/\\1.qmd", config_lines)
+  config_lines <- gsub("- chapters/index.qmd", "- index.qmd", config_lines)
+  brio::write_lines(config_lines, file.path(parent_dir, project_dir, "_quarto.yml"))
+
+  withr::with_dir(parent_dir, render_book(project_dir))
+  expect_true(fs::dir_exists(file.path(parent_dir, project_dir, "_book")))
+
+  index <- xml2::read_html(file.path(parent_dir, project_dir, "_book", "chapters", "references.html"))
+  spanish_link <- xml2::xml_find_first(index, '//a[@id="language-link-es"]')
+  expect_equal(xml2::xml_attr(spanish_link, "href"), "https://example.com/es/chapters/references.es.html")
+})
+
+
 test_that("render_website() works", {
   withr::local_envvar("BABELQUARTO_TESTS_URL" = "true")
 
