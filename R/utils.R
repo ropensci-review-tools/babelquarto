7dcc9f26c7d42037fa62ac1a3db1d8dda0669b6b
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-08-26 16:21:43 +0200

    chore: lint

--- R/utils.R
+++ R/utils.R
@@ -5,8 +5,8 @@ replace_true_false <- function(list) {
     list <- lapply(list, replace_true_false)
   } else if (is.logical(list)) {
     list <- as.character(list)
-    list <- gsub("TRUE", "true", list)
-    list <- gsub("FALSE", "false", list)
+    list <- gsub("TRUE", "true", list, fixed = TRUE)
+    list <- gsub("FALSE", "false", list, fixed = TRUE)
     # Set class of this character vector to 'verbatim'
     # so that yaml::write_yaml() will not add quotation marks
     class(list) <- "verbatim"
@@ -16,12 +16,13 @@ replace_true_false <- function(list) {
 
 lang_code_chapter_list <- function(chapters_list, language_code) {
   chapters_list <- gsub(
-    "\\.Rmd", sprintf(".%s.Rmd", language_code),
+    "\\.Rmd", # nolint: fixed_regex_linter
+    sprintf(".%s.Rmd", language_code),
     chapters_list
   )
 
   gsub(
-    "\\.qmd",
+    "\\.qmd", # nolint: fixed_regex_linter
     sprintf(".%s.qmd", language_code),
     chapters_list
   )
