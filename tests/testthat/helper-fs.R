24a35c5d2e0c47943aa5682643e9b728e5f3fb75
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-08-28 13:37:42 +0200

    test: namespace expect_ to satisfy lintr

--- tests/testthat/helper-fs.R
+++ tests/testthat/helper-fs.R
@@ -1,11 +1,11 @@
 expect_dir_exists <- function(path) {
-  expect_true(fs::dir_exists(path))
+  testthat::expect_true(fs::dir_exists(path))
 }
 
 expect_file_exists <- function(path) {
-  expect_true(fs::file_exists(path))
+  testthat::expect_true(fs::file_exists(path))
 }
 
 expect_dir_absent <- function(path) {
-  expect_false(fs::dir_exists(path))
+  testthat::expect_false(fs::dir_exists(path))
 }
