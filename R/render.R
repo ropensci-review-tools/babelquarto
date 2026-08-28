08790018c3f505ee214adead0d991204eb4a6f41
hanteng <hanteng@gmail.com>
2026-08-28 17:52:30 +0800

    Revert "fix(render): sanitize nested yaml data.frames before modifyList in add_links"
    This reverts commit 1d50aec262aaf890bfdde01b4c2cb871d3fd3540.
    
     Changes to be committed:
    	modified:   R/render.R

--- R/render.R
+++ R/render.R
@@ -569,17 +569,8 @@ add_links <- function(
     input = project_dir,
     profile = c(language_code, profile)
   )
-
   lang_config <- q_inspect$config
-
-# Helper to recursively convert nested data.frames back to lists
-  df_to_list <- function(x) {
-    if (is.data.frame(x)) x <- as.list(x)
-    if (is.list(x)) x <- lapply(x, df_to_list)
-    x
-  }
-
-  config <- utils::modifyList(df_to_list(config), df_to_list(lang_config))
+  config <- utils::modifyList(config, lang_config)
 
   codes <- read_lang_codes(config)
   current_lang <- purrr::keep(codes, ~ .x[["name"]] == language_code)
